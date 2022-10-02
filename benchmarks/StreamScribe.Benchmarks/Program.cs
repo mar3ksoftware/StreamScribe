cc38f36ee79fe469a3917f427eb04e96321a633b
Marcin Janczak <mardpl@aol.com>
2022-10-02 23:41:01 +0200

    [+] Added benchmarks for MemoryPartitioner.

--- benchmarks/StreamScribe.Benchmarks/Program.cs
+++ benchmarks/StreamScribe.Benchmarks/Program.cs
@@ -1,8 +1,13 @@
-﻿namespace StreamScribe.Benchmarks;
+﻿using BenchmarkDotNet.Running;
+
+using StreamScribe.Benchmarks.Chunk;
+
+namespace StreamScribe.Benchmarks;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
+        _ = BenchmarkRunner.Run(typeof(MemoryPartitionerBenchmarks).Assembly);
     }
 }
