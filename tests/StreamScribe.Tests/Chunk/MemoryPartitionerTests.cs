91f9e23f44a0cb1fddbdfd1765e2d533f9b54711
Marcin Janczak <mardpl@aol.com>
2022-10-02 22:52:47 +0200

    [~] Added memory partitoiner tests.

---
+++ tests/StreamScribe.Tests/Chunk/MemoryPartitionerTests.cs
@@ -0,0 +1,23 @@
+﻿using StreamScribe.Chunk;
+using StreamScribe.Tests.Attributes;
+
+namespace StreamScribe.Tests.Chunk;
+
+public sealed class MemoryPartitionerTests
+{
+    [Theory]
+    [ScribeAutoData]
+    public void GenerateChunks_Test(int size, IEnumerable<byte> source)
+    {
+        var chunks = MemoryPartitioner.GenerateChunks(size, source);
+        Assert.NotEmpty(chunks);
+    }
+
+    [Theory]
+    [ScribeAutoData]
+    public async Task GenerateChunksAsync_Test(int size, IEnumerable<byte> source)
+    {
+        var chunks = await MemoryPartitioner.GenerateChunksAsync(size, source);
+        Assert.NotEmpty(chunks);
+    }
+}
